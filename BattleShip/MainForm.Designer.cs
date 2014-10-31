4402b01181511edd46c9b5d1b7390d4e7f1a5e60
Zakrevskijj <lirik1105@gmail.com>
2014-10-31 23:28:42 +0200

    Buttons(Kost)

---
+++ BattleShip/MainForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace BattleShip
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 311);
+            this.Name = "MainForm";
+            this.Text = "BattleShip Online";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
+
---
+++ BattleShip/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattleShip
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control[,] userGrid = new Button[10, 10];
+            Control[,] pcGrid=new Button[10,10];
+            for(int i= 0;i<10;i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    pcGrid[i,j]=new Button();
+                    this.Controls.Add(pcGrid[i, j]);
+                    pcGrid[i, j].Size=new Size(25,25);
+                    pcGrid[i, j].Location = new Point(j * 25 + 325, i * 25 + 25);
+                    userGrid[i, j] = new Button();
+                    this.Controls.Add(userGrid[i,j]);
+                    userGrid[i,j].Size= new Size(25,25);
+                    userGrid[i, j].Location = new Point(j*25+25, i*25+25);
+                }
+        }
+    }
+}
